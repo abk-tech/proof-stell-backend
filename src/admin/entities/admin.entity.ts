8932a580fb1efad311e2c4b69e7af45787c6caae
Qoder-Undefined <133337507+Nanafancy@users.noreply.github.com>
2026-06-13 19:34:23 +0100

    updated the admin

--- src/admin/entities/admin.entity.ts
+++ src/admin/entities/admin.entity.ts
@@ -1,1 +1,9 @@
-export class Admin {}
+export class Admin {
+
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    name: string;   
+
+    @Column({ type: 'varchar', length: 255, nullable: false })
+    email: string;
+}
